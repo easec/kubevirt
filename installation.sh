31af4068ae31c0a669b67935695b82fdc8ce11be
easec <mats.johannesson@easec.se>
2021-04-17 12:12:35 +0200

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,14 @@
+export KUBEVIRT_VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases/latest | jq -r .tag_name)
+echo $KUBEVIRT_VERSION
+
+kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/kubevirt-operator.yaml
+
+kubectl create configmap kubevirt-config -n kubevirt --from-literal debug.useEmulation=true
+
+kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/kubevirt-cr.yaml
+
+kubectl get pods -n kubevirt
+
+wget -O virtctl https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/virtctl-${KUBEVIRT_VERSION}-linux-amd64
+
+sudo chmod +x virtctl
